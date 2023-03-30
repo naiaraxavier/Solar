a70763fc9636b5a0af4ee6109c7887e4c52207a7
Naiara Martins <naiaraxf@gmail.com>
2023-03-30 15:22:15 -0300

    Requisito 5

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,6 @@ import React, { Component } from 'react';
 export default class Title extends Component {
   render() {
     const { headline } = this.props;
-    console.log(headline);
     return (
       <h2>{headline}</h2>
     );
